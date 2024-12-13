9181dde6ffd9fe8d14d01ca36360f87d779f33bb
Leonardo Mosimann Conti <leomconti@gmail.com>
2024-12-12 23:46:12 -0300

    Initial Commit

---
+++ llama chat/llama_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  llama_chatApp.swift
+//  llama chat
+//
+//  Created by Leonardo Mosimann conti on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct llama_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ llama chatTests/llama_chatTests.swift
@@ -0,0 +1,17 @@
+//
+//  llama_chatTests.swift
+//  llama chatTests
+//
+//  Created by Leonardo Mosimann conti on 12/12/24.
+//
+
+import Testing
+@testable import llama_chat
+
+struct llama_chatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
